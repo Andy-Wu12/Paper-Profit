2900eeab73570acb4df63d8df39bb5da3f9793e0
Andy Wu <awpats12@gmail.com>
2022-10-11 18:20:57 -0400

    (#3) - Checkbox component to toggle between signup and login forms

--- client/pages/index.js
+++ client/pages/index.js
@@ -44,9 +44,13 @@ export default function Home() {
 
 function ToggleableForm({isLogin, handleClick}) {
   return (
-    <div className='toggleSwitch'>
-      <label for="toggleForm"> Need to sign up? </label>
-      <input id="toggleForm" type="checkbox" checked={isLogin} onClick={handleClick} />
-    </div>
+    <>
+      <div className='toggleSwitch'>
+        <label htmlFor="toggleForm"> Already Signed Up? </label>
+        <input id="toggleForm" type="checkbox" checked={isLogin} onChange={handleClick} />
+      </div>
+
+      {isLogin ? <forms.LoginForm /> : <forms.SignupForm />}
+    </>
   )
 }

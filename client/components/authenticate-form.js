8afe45fa8b19d3d582bfd50d23a462c1d19a8337
Andy Wu <awpats12@gmail.com>
2022-10-11 15:57:52 -0400

    (#3) - Login form layout completed

---
+++ client/components/authenticate-form.js
@@ -0,0 +1,18 @@
+import styles from '../styles/Home.module.css'
+import formStyles from '../styles/forms.module.css'
+
+export function LoginForm() {
+  return (
+    <>
+      <h2> Login </h2>
+      <form className={styles.card} method="POST" action="/login">
+        <label className={formStyles.label} htmlFor="username"> Username </label>
+        <input name="username" className={formStyles.formInput} /><br/><br/>
+        <label className={formStyles.label} htmlFor="password"> Password </label>
+        <input name="password" className={formStyles.formInput} /><br/><br/>
+      </form>
+    </>
+  )
+}
+
+export default LoginForm;

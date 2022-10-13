9cfdf3c3f013242dcb368b8301df5452824e7614
Andy Wu <awpats12@gmail.com>
2022-10-12 20:10:01 -0400

    (#8) - Refactored authentication form to work with an arbitrary number of fields

--- client/components/authenticate-form.js
+++ client/components/authenticate-form.js
@@ -1,15 +1,21 @@
 import styles from '../styles/Home.module.css'
 import formStyles from '../styles/forms.module.css'
 
-function AuthenticationForm({header, postRoute}) {
+function AuthenticationForm({header, postRoute, fields}) {
+  const formFields = fields.map((field) => {
+    return (
+    <>
+      <label className={formStyles.label} htmlFor={field}> {field} </label>
+      <input id={field} name={field} className={formStyles.formInput} /><br/><br/>
+    </>);
+  });
+
+  console.log(formFields);
   return (
     <>
       <h2> {header} </h2>
       <form className={styles.card} method="POST" action={postRoute}>
-        <label className={formStyles.label} htmlFor="username"> Username </label>
-        <input id="username" name="username" className={formStyles.formInput} /><br/><br/>
-        <label className={formStyles.label} htmlFor="password"> Password </label>
-        <input id="password" name="password" className={formStyles.formInput} /><br/><br/>
+        {formFields}
         <button className={formStyles.button} type='submit'> {header} </button>
       </form>
     </>
@@ -17,14 +23,17 @@ function AuthenticationForm({header, postRoute}) {
 }
 
 export function LoginForm() {
+  const fields = ["Email", "Password"];
+
   return (
-    <AuthenticationForm header="Login" postRoute="/login" />
+    <AuthenticationForm header="Login" postRoute="/login" fields={fields} />
   )
 }
 
 export function SignupForm() {
+  const fields = ["Username", "Email", "Password"]
   return (
-    <AuthenticationForm header="Sign Up" postRoute="/signup" />
+    <AuthenticationForm header="Sign Up" postRoute="/signup" fields={fields} />
   )
 }
 

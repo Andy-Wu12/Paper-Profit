e89fb8b50176d4f248ad05234d7435db43f84423
Andy Wu <awpats12@gmail.com>
2022-10-23 00:39:57 -0400

    (#31) - User balance updated in real-time

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import Koa from 'koa';
 import Router from '@koa/router';
 import koaBody from 'koa-body';
-// import session from 'koa-session';
+import websockify from 'koa-websocket';
 
 import cors from '@koa/cors';
 import dotenv from 'dotenv';
@@ -11,33 +11,15 @@ import userRouter from './routes/users.js';
 import authRouter from './routes/auth.js';
 import stockRouter from './routes/stock.js';
 import transactionRouter from './routes/transaction.js';
+import wsRouter from './routes/websockets.js';
 
-const app = new Koa();
+const app = websockify(new Koa());
 const router = new Router();
 const port = 3011;
 dotenv.config();
 
 const uri = process.env.MONGO_URI;
 mongoose.connect(uri);
-
-//  Koa sessions
-// app.keys = ['secret'];
-
-// const CONFIG = {
-//   key: 'koa.sess', /** (string) cookie key (default is koa.sess) */
-//   /** (number || 'session') maxAge in ms (default is 1 days) */
-//   /** 'session' will result in a cookie that expires when session/browser is closed */
-//   /** Warning: If a session cookie is stolen, this cookie will never expire */
-//   maxAge: 86400000,
-//   autoCommit: true, /** (boolean) automatically commit headers (default true) */
-//   overwrite: true, /** (boolean) can overwrite or not (default true) */
-//   httpOnly: true, /** (boolean) httpOnly or not (default true) */
-//   signed: true, /** (boolean) signed or not (default true) */
-//   rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
-//   renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
-//   secure: false, /** (boolean) secure cookie*/
-//   sameSite: null, /** (string) session cookie sameSite options (default null, don't set it) */
-// };
 
 app
 	// .use(session(CONFIG, app))
@@ -82,6 +64,8 @@ app.use(async (ctx, next) => {
 	ctx.set('X-Response-Time', `${ms}ms`);
 });
 
+// Websocket routes
+app.ws.use(wsRouter.routes());
 
 app.use(router.routes());
 app.use(userRouter.routes());

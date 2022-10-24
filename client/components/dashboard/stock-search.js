b5732ea94a47e5a7b741092dca3c281a06642f24
Andy Wu <awpats12@gmail.com>
2022-10-23 23:54:24 -0400

    (#31) - Stock search detail pricing refactored to use TD Ameritrade websocket

--- client/components/dashboard/stock-search.js
+++ client/components/dashboard/stock-search.js
@@ -1,13 +1,57 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import styles from '../../styles/Home.module.css'
+
+// Ameritrade websocket stuff
+import userPrincipals from '../../pages/api/userprincipal.json'
+import CreateWebsocket from '../generic/ameritrade-websocket'
 
 export default function StockSearchForm({setStockData, setShowHoldings, setIsLoading}) {  
   const router = useRouter();
+
+  const [websocket, setWebsocket] = useState(null);
+
+  const userPrincipalsResponse = userPrincipals;
+  const stockSubRequest = (symbol) => {
+    return {"requests": [
+      {
+        "service": "QUOTE",
+        "command": "SUBS",
+        "requestid": 1,
+        "account": userPrincipalsResponse.accounts[0].accountId,
+        "source": userPrincipalsResponse.streamerInfo.appId,
+        "parameters": {
+            "keys": symbol,
+            "fields": "0,1,2"
+        }
+      }
+    ]}
+  };
+
+  useEffect(() => {
+    try {
+      if(!websocket) {
+        const socket = CreateWebsocket({
+          onMessage: (message) => {
+            const data = JSON.parse(message.data);
+            if(data.data) {
+              setStockData(data.data[0].content[0]);
+            }
+          }
+       });
+        setWebsocket(socket);
+      }
+    } catch (e) {
+      // setWebsocket(null);
+      setTest(false);
+    }
+  }, []);
 
   async function handleSubmit(e) {
     e.preventDefault();
     setIsLoading(true);
     const tickerSymbol = e.target.ticker.value;
+    websocket.send(JSON.stringify(stockSubRequest(tickerSymbol)));
     if(!tickerSymbol) {
       // Another option is to setStockData(null) to show error in StockDetail component, 
       // but this may be against good UX
@@ -15,9 +59,9 @@ export default function StockSearchForm({setStockData, setShowHoldings, setIsLoa
     }
 
     router.push(`/dashboard/?symbol=${tickerSymbol.toUpperCase()}`);
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/stock-info/${tickerSymbol}`);
-    const stockData = await response.json();
-    setStockData(stockData);
+    // const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/stock-info/${tickerSymbol}`);
+    // const stockData = await response.json();
+    // setStockData(stockData);
     setShowHoldings(false);
     setIsLoading(false);
   }

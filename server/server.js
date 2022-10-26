7b18087399929f06f7fd633407e700fdfb838e5f
Andy Wu <awpats12@gmail.com>
2022-10-26 14:10:24 -0400

    Removed some logging from server, added heartbeats and timeouts to /balance websocket

--- server/server.js
+++ server/server.js
@@ -51,11 +51,11 @@ app.use(async (ctx, next) => {
 	await next();
 });
 
-app.use(async (ctx, next) => {
-	await next();
-	const rt = ctx.response.get('X-Response-Time');
-	console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-});
+// app.use(async (ctx, next) => {
+// 	await next();
+// 	const rt = ctx.response.get('X-Response-Time');
+// 	console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+// });
 
 app.use(async (ctx, next) => {
 	const start = Date.now();

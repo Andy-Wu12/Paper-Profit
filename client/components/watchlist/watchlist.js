9c94d9c3bea50cafe7c68919a0f5519affb1074c
Andy Wu <awpats12@gmail.com>
2022-11-06 15:30:31 -0500

    (#50) - Added more data to show on watchlist grid

--- client/components/watchlist/watchlist.js
+++ client/components/watchlist/watchlist.js
@@ -9,7 +9,7 @@ import Ameritrade from '../generic/ameritrade-websocket';
 export default function WatchList({websocket, watchListData, ...setterProps}) {  
   const [realtimeData, setRealtimeData] = useState(null);
 
-  const subscriptionFields = "0,1";
+  const subscriptionFields = "0,1,2,3,8,30,31";
 
   useEffect(() => {
     websocket.onmessage = (message) => {
@@ -50,12 +50,42 @@ export default function WatchList({websocket, watchListData, ...setterProps}) {
 export function WatchlistGrid({websocket, realtimeData, ...setterProps}) {
   const watchListHTML = [];
   
+  const fieldToLabelMap = {
+    '1': 'Bid',
+    '2': 'Ask',
+    '3': 'Last',
+    '8': 'Volume',
+    '30': '52 High',
+    '31': '52 Low',
+  }
+
+  const tableHeaders = [];
+
+  // Map headers
+  for(const [,label] of Object.entries(fieldToLabelMap)) {
+    tableHeaders.push(<th key={`${label}-label`}> {label} </th>);
+  }
+
+  // Map all fields for each symbol
   realtimeData.map((symbolData) => {
+    const bid = symbolData['1'];
+    const ask = symbolData['2'];
+    const mark = (bid + ask) / 2;
+
     const symbolHTML = (
       <tr key={`${symbolData.key}-watchlist-row`} className={gridStyles.gridRow}>
         <td> <StockSymbolButton {...setterProps} websocket={websocket} symbol={symbolData.key} />  </td>
         {/* TODO: Color should change depending on previous price (red down, green up)  */}
-        <td className={quoteStyles.askPrice}> {symbolData['1']} </td>
+        <td> {mark.toFixed(2)} </td>
+        {Object.keys(fieldToLabelMap).map((key) => {
+          const fieldName = fieldToLabelMap[key];
+          return (
+            <td key={`${symbolData.key}-${fieldName}`}>
+              {/* Don't show decimal points for volume */}
+              {(fieldName === 'Volume') ? symbolData[key] : symbolData[key].toFixed(2) || '-'} 
+            </td>
+          )
+        })}
       </tr>
     );
     // console.log(symbolHTML);
@@ -67,7 +97,8 @@ export function WatchlistGrid({websocket, realtimeData, ...setterProps}) {
       <tbody>
         <tr className={gridStyles.gridRow}>
           <th> Position </th>
-          <th> Trade Price </th>
+          <th> Mark </th>
+          {tableHeaders}
         </tr>
         {watchListHTML}
       </tbody>

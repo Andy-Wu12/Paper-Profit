2f06d2976008ce690192e81febc8b81ed4d01388
Andy Wu <awpats12@gmail.com>
2023-03-26 18:37:15 -0400

    Temporarily remove earnings charts until api comes back online

--- client/components/dashboard/charts/stockCharts.tsx
+++ client/components/dashboard/charts/stockCharts.tsx
@@ -5,9 +5,9 @@ import QuarterlyEarningsChart from "./quarterlyEarningsChart"
 export default function StockCharts(): React.ReactElement {
   return (
     <div className='stockChartsContainer'>
-      <PeriodPriceChart /> <br/><br/>
-      <YearlyEarningsGraph /> <br/>
-      <QuarterlyEarningsChart /> <br/>
+      <PeriodPriceChart /> <br/>
+      {/* <YearlyEarningsGraph /> <br/> */}
+      {/* <QuarterlyEarningsChart /> <br/> */}
     </div>
   )
 }
